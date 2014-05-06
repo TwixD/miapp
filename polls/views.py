1faa38bcabd5d13125fe6f4f93d09b4bb2529b3b
TwixD <juanDavidCRivera@hotmail.com>
2014-05-05 20:39:37 -0500

    Final

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,10 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from polls.models import Poll, Choice
+from django.views import generic
 from django.core.urlresolvers import reverse
 # Create your views here.
+'''
 def index(request):
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
 	context = {'latest_poll_list': latest_poll_list}
@@ -16,6 +18,23 @@ def detail(request, poll_id):
 def results(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/results.html', {'poll': poll})
+'''
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
